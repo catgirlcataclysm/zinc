6677936023ed2cb093675a87635eadd58f92151c
kaitlynkittyy <kaitlynyaadev@kaitlynyaa.dev>
2024-07-14 22:59:42 -0400

    s

--- src/terminal/ui.rs
+++ src/terminal/ui.rs
@@ -1,4 +1,8 @@
+use cursive::views::TextView;
+
 pub fn run() {
     let mut zinc = cursive::default();
     zinc.add_global_callback('q', |z| z.quit());
+    zinc.add_layer(TextView::new("hi goobabs"));
+    zinc.run();
 }
