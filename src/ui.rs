5261b9bc983f0b0a484ac8cf3af4a4fd71693953
kaitlynkittyy <kaitlynyaadev@kaitlynyaa.dev>
2024-07-18 18:39:29 -0400

    finally

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,6 @@
 #[ path = "install.rs"] mod install;
 
-use std::{fs::{read_dir, DirEntry, ReadDir}, io::Error};
+use std::{fs::{read_dir, DirEntry, ReadDir}, io::Error, mem};
 
 use cursive::{view::{Nameable, Resizable}, views::{Button, EditView, LinearLayout, NamedView, PaddedView, Panel, RadioButton, RadioGroup, TextView}, Cursive};
 use cursive_tabs::TabPanel;
@@ -60,68 +60,76 @@ let logo = TextView::new(
     zinc.run();
 }
 
-fn find_devices(_z: &mut Cursive) {
+fn find_devices(z: &mut Cursive) {
     let dev = read_dir("/dev").expect("Failed to get list of storage devices.");
-    let paths: Result<Vec<DirEntry>, Error> = dev.into_iter().collect();
-
-
+    for path in dev {
+        if let Ok(path) = path {
+            if path.path().to_string_lossy().contains("/dev/mmcblk") {
+                if path.path().to_string_lossy().contains("boot0") {
+                    let device = path.path().to_string_lossy().replace("boot0", "");
+                    config(z, device);
+                } else {
+                    let device = path.path().to_string_lossy().into_owned();
+                    config(z, device);
+                }
+            }
+        }
     }
+}
 
 
-//fn config(z: &mut Cursive) {
-//
-//    let tabs = TabPanel::new()
-//        .with_tab(NamedView::new("Device", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
-//            .child(RadioButton::global("device", , label)))))
-//        .with_tab(NamedView::new("Distro", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
-//            .child(RadioButton::global("distro", Distro::ArchLinux, "Arch Linux"))
-//            .child(RadioButton::global("distro", Distro::Debian, "Debian"))
-//            .child(RadioButton::global("distro", Distro::Void, "Void Linux"))
-//            .child(RadioButton::global("distro", Distro::VoidMusl, "Void Musl"))
-//            .child(RadioButton::global("distro", Distro::Gentoo, "Gentoo")))))
-//        .with_tab(NamedView::new("Filesystem", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
-//            .child(RadioButton::global("fs", Filesystem::F2FS, "F2FS"))
-//            .child(RadioButton::global("fs", Filesystem::Ext4, "Ext4"))
-//            .child(RadioButton::global("fs", Filesystem::Btrfs, "Btrfs")))))
-//        .with_tab(NamedView::new("Desktop", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
-//            .child(RadioButton::global("desktop", Desktop::KDE, "KDE Plasma"))
-//            .child(RadioButton::global("desktop", Desktop::GNOME, "Gnome"))
-//            .child(RadioButton::global("desktop", Desktop::Sway, "Sway"))
-//            .child(RadioButton::global("desktop", Desktop::XFCE, "XFCE")))))
-//        .with_tab(NamedView::new("Accounts", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
-//            .child(TextView::new("Enter the root password:"))
-//            .child(EditView::new().with_name("rootpasswd").fixed_height(1))
-//            .child(TextView::new("Enter your Username:"))
-//            .child(EditView::new().with_name("username").fixed_height(1))
-//            .child(TextView::new("Enter your Password:"))
-//            .child(EditView::new().with_name("passwd").fixed_height(1))
-//            .child(PaddedView::lrtb(10, 0, 0, 0, Button::new("Finish", finish))))));
-//            
-//
-//    z.pop_layer();
-//    z.add_layer(tabs);
-//}
-//
-//fn finish(z: &mut Cursive) {
-//    // TODO: handle user not inputting username and password
-//    let distro = *RadioGroup::<Distro>::with_global("distro", |distro| distro.selection().clone());
-//    let fs = *RadioGroup::<Filesystem>::with_global("fs", |fs| fs.selection().clone());
-//    let desktop = *RadioGroup::<Desktop>::with_global("desktop", |de| de.selection().clone());
-//    let username = z.call_on_name("username", |view: &mut EditView| view.get_content()).unwrap().to_string();
-//    let passwd = z.call_on_name("passwd", |view: &mut EditView| view.get_content()).unwrap().to_string();
-//    let rootpasswd = z.call_on_name("rootpasswd", |view: &mut EditView| view.get_content()).unwrap().to_string();
-//
-//    let selection = Selections {
-//        device,
-//        distro,
-//        fs,
-//        desktop,
-//        rootpasswd,
-//        username,
-//        passwd
-//    };
-//
-//    z.pop_layer();
-//
-//    install::cgpt_tomfoolery(selection);
-//}
+fn config(z: &mut Cursive, device: String) {
+
+    let tabs = TabPanel::new()
+        .with_tab(NamedView::new("Distro", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
+            .child(RadioButton::global("distro", Distro::ArchLinux, "Arch Linux"))
+            .child(RadioButton::global("distro", Distro::Debian, "Debian"))
+            .child(RadioButton::global("distro", Distro::Void, "Void Linux"))
+            .child(RadioButton::global("distro", Distro::VoidMusl, "Void Musl"))
+            .child(RadioButton::global("distro", Distro::Gentoo, "Gentoo")))))
+        .with_tab(NamedView::new("Filesystem", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
+            .child(RadioButton::global("fs", Filesystem::F2FS, "F2FS"))
+            .child(RadioButton::global("fs", Filesystem::Ext4, "Ext4"))
+            .child(RadioButton::global("fs", Filesystem::Btrfs, "Btrfs")))))
+        .with_tab(NamedView::new("Desktop", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
+            .child(RadioButton::global("desktop", Desktop::KDE, "KDE Plasma"))
+            .child(RadioButton::global("desktop", Desktop::GNOME, "Gnome"))
+            .child(RadioButton::global("desktop", Desktop::Sway, "Sway"))
+            .child(RadioButton::global("desktop", Desktop::XFCE, "XFCE")))))
+        .with_tab(NamedView::new("Accounts", PaddedView::lrtb(2, 2, 2, 2, LinearLayout::vertical()
+            .child(TextView::new("Enter the root password:"))
+            .child(EditView::new().with_name("rootpasswd").fixed_height(1))
+            .child(TextView::new("Enter your Username:"))
+            .child(EditView::new().with_name("username").fixed_height(1))
+            .child(TextView::new("Enter your Password:"))
+            .child(EditView::new().with_name("passwd").fixed_height(1))
+            .child(PaddedView::lrtb(10, 0, 0, 0, Button::new("Finish", move |z| finish(z, device.clone())))))));
+            
+
+    z.pop_layer();
+    z.add_layer(tabs);
+}
+
+fn finish(z: &mut Cursive, device: String) {
+    // TODO: handle user not inputting username and password
+    let distro = *RadioGroup::<Distro>::with_global("distro", |distro| distro.selection().clone());
+    let fs = *RadioGroup::<Filesystem>::with_global("fs", |fs| fs.selection().clone());
+    let desktop = *RadioGroup::<Desktop>::with_global("desktop", |de| de.selection().clone());
+    let username = z.call_on_name("username", |view: &mut EditView| view.get_content()).unwrap().to_string();
+    let passwd = z.call_on_name("passwd", |view: &mut EditView| view.get_content()).unwrap().to_string();
+    let rootpasswd = z.call_on_name("rootpasswd", |view: &mut EditView| view.get_content()).unwrap().to_string();
+
+    let selection = Selections {
+        device,
+        distro,
+        fs,
+        desktop,
+        rootpasswd,
+        username,
+        passwd
+    };
+
+    z.pop_layer();
+
+    install::cgpt_tomfoolery(selection);
+}
