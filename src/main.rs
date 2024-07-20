901987480fd1973f811d7511bb6b7599c94aba87
Radical <radical@radical.fun>
2024-07-20 23:14:16 +0200

    d
    d

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod hardware;
 
 pub const BOARDS: [&'static str; 10] = ["bob", "coachz", "hana", "homestar", "kevin", "kodama", "krane", "lazor", "minnie", "speedy"];
 pub const BASEBOARDS: [&'static str; 5] = ["gru", "kukui", "oak", "trogdor", "veyron"];
+pub const ARCH: Option<&'static str> = option_env!("CARGO_CFG_TARGET_ARCH");
 
 fn main() {
     Command::new("nmtui").arg("connect").status().expect("Failed to query NetworkManager.");
